7b84aab14a33bdadbd41f2ae6654c0f47b75e5d1
Zimbioz1s <a10705980@tec.mx>
2024-04-08 19:43:41 -0600

    Agregar los nuevos entregables

--- Extraccion.py
+++ Extraccion.py
@@ -19,3 +19,27 @@ filtro2.to_csv('Entregable2.csv')
 filtro3 = df.iloc[2:8, :]
 print(filtro3)
 filtro3.to_csv('Entregable3.csv')
+
+#Filtro por fecha
+filtro4=df.loc[["2022-11-22","'2022-12-23"], ["SUBTOTAL_PARTIDA"]]
+filtro4.to_csv('Entregable4.csv')
+
+#Filtro primeros 5
+filtro5=df.head(8)
+filtro5.to_csv('Entregable5.csv')
+
+#Filtro mayores a 77000
+filtro6=df[df["SUBTOTAL_PARTIDA"] > 77000]
+filtro6.to_csv('Entregable6.csv')
+
+#Filtro precio y fecha
+filtro7=df[(df["SUBTOTAL_PARTIDA"] > 77000) & (df["FECHA_DOC"] == "2022-05-24")]
+filtro7.to_csv('Entregable7.csv')
+
+#Filtro precio y fecha 2
+filtro8=df[(df["SUBTOTAL_PARTIDA"] > 77000)| (df["FECHA_DOC"] == "2022-05-24")]
+filtro8.to_csv('Entregable8.csv')
+
+#Filtro not
+filtro9=df[~(df["SUBTOTAL_PARTIDA"] > 77000) & ~(df["FECHA_DOC"] == "2022-05-24")]
+filtro9.to_csv('Entregable9csv')
